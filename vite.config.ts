ec6cbc77409c314fd3fa0b6ffde93dbcf629b4a1
rakesh <rakesh@aiyensi.com>
2025-06-10 11:57:19 +0530

    Build for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/',
+  build: {
+    outDir: 'docs'  // 👈 GitHub will serve this folder
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
